7172d88451808a13c31c4ebf4a9d03f86dee076e
Ernster24 <ernieg2409@gmail.com>
2023-11-24 13:00:57 +0000

    First Commit

---
+++ src/audio_transcript/transcript.py
@@ -0,0 +1,16 @@
+import speech_recognition as speech
+import glob
+
+r = speech.Recognizer()
+
+audio_set = glob.glob("Test_Audio/*")
+
+for audio_file in audio_set:
+    with speech.AudioFile(audio_file) as source:
+        audio_data = r.record(source)
+        text = r.recognize_google(audio_data)
+        print("Writing '" + text + "' to file...")
+        file_no = str(audio_set.index(audio_file))
+        transcript = open("transcript.txt", "a")
+        transcript.write("[" + file_no + "] " + text + "\n")
+        transcript.close()  
